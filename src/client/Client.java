46bc19eb16f3718ab31f5782c7c1e1c5c41c472e
pranay-bhatkar <pranaybhatkar81@gmail.com>
2024-10-11 01:28:47 +0530

    issue solved

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,31 @@
+
 package client;
 
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+
+class ClientThread extends Thread {
+    private Socket socket;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+
+            String serverMessage;
+            while ((serverMessage = reader.readLine()) != null) {
+                System.out.println(serverMessage);
+            }
+        } catch (IOException e) {
+            System.out.println("Client exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
 
 public class Client {
     private static final String HOSTNAME = "localhost";
@@ -15,8 +38,8 @@ public class Client {
             new ClientThread(socket).start();
 
             try (OutputStream output = socket.getOutputStream();
-                 PrintWriter writer = new PrintWriter(output, true);
-                 Scanner scanner = new Scanner(System.in)) {
+                    PrintWriter writer = new PrintWriter(output, true);
+                    Scanner scanner = new Scanner(System.in)) {
 
                 String userMessage;
                 do {
@@ -24,28 +47,6 @@ public class Client {
                     userMessage = scanner.nextLine();
                     writer.println(userMessage);
                 } while (!userMessage.equalsIgnoreCase("exit"));
-            }
-        } catch (IOException e) {
-            System.out.println("Client exception: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-}
-
-class ClientThread extends Thread {
-    private Socket socket;
-
-    public ClientThread(Socket socket) {
-        this.socket = socket;
-    }
-
-    public void run() {
-        try (InputStream input = socket.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
-
-            String serverMessage;
-            while ((serverMessage = reader.readLine()) != null) {
-                System.out.println(serverMessage);
             }
         } catch (IOException e) {
             System.out.println("Client exception: " + e.getMessage());
